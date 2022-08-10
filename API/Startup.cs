066fc1a62a982e10136bb9088e2e53b54ff36749
Sonal <mahendra.sonal@gmail.com>
2022-08-10 17:39:20 +0530

    Angular project

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,10 +28,11 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-services.AddDbContext<DataContext>(options=>{
-    options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            services.AddDbContext<DataContext>(options=> {
+            options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
 });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -51,7 +52,9 @@ services.AddDbContext<DataContext>(options=>{
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(x=> {
+                x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithOrigins("https://localhost:4200");
+            });
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
