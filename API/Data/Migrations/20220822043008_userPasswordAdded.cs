7dac94efb28e1ebe861674d6825862ee9b261d79
Sonal <mahendra.sonal@gmail.com>
2022-08-29 12:44:20 +0530

    End of Section 4-AuthenticationJWT

---
+++ API/Data/Migrations/20220822043008_userPasswordAdded.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class userPasswordAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "passwordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "passwordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "passwordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "passwordSalt",
+                table: "Users");
+        }
+    }
+}
